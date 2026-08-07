d494a4c0c8bc889411a7f2e10fb6a0a59d4a2501
a1969651 <a1969651@adelaide.edu.au>
2026-08-07 11:18:04 +0930

    Add files via upload

---
+++ Week_1/Prac_A/PayrollCalculator/PayrollCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter employee name: ");
+        string name = Console.ReadLine();
+
+        try
+        {
+            Console.WriteLine("Enter hours worked: ");
+            double hours = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter rate: ");
+            double rate = double.Parse(Console.ReadLine());
+
+            double net_pay = CalculatePay.calculate_pay(hours, rate);
+            Console.WriteLine($"{name} earnt {net_pay}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Input");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
