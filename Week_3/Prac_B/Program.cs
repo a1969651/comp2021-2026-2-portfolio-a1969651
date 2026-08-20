96f69aa53b43f25bede2701f0c1a874eb189b81b
a1969651 <a1969651@adelaide.edu.au>
2026-08-20 14:41:45 +0930

    Add files via upload

---
+++ Week_3/Prac_B/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+
+public abstract class Employee {
+    public const decimal TaxRate = 0.2m;
+
+    public abstract string Name {get; set;}
+
+    public abstract decimal CalculatePay();
+}
+
+public interface IReportable
+{
+    string GenerateReport();
+}
+
+public class FullTimeEmployee : Employee, IReportable
+{
+    public decimal AnnualSalary {get; set;}
+    public override string Name {get; set;}
+
+    public override decimal CalculatePay()
+    {
+        return AnnualSalary - (AnnualSalary * TaxRate);
+    }
+
+    public string GenerateReport()
+    {
+        return $"{Name}, {AnnualSalary}, {CalculatePay()}";
+    }
+}
+
+public class Contractor : Employee, IReportable
+{
+    public decimal Rate {get; set;}
+    public decimal Hours {get; set;}
+    public override string Name {get; set;}
+
+    public override decimal CalculatePay()
+    {
+        return (Rate * Hours) - TaxRate;
+    }
+
+    public string GenerateReport()
+    {
+        return $"{Name}, {Rate}, {Hours}, {CalculatePay()}";
+    }
+}
+
+
+public class Run
+{
+    static void Main()
+    {
+        FullTimeEmployee Greg = new FullTimeEmployee();
+        Greg.Name = "Greg";
+        Greg.AnnualSalary = 80000m;
+        Console.WriteLine(Greg.GenerateReport());
+    }
+}
