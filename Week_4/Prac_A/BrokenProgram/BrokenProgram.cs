ee51efc538a214db9ff9d25f98c15f09746f93ac
a1969651 <a1969651@adelaide.edu.au>
2026-08-25 11:06:13 +1000

    Add files via upload

---
+++ Week_4/Prac_A/BrokenProgram/BrokenProgram.cs
@@ -0,0 +1,40 @@
+﻿
+public static class BrokenProgram
+{
+  static void Main(string[] args)
+  {
+    const string input = "23 + 77 + 3 + 457 * 2";
+    int total = 0;
+    List<int> items = [];
+    List<string> symbols = [];
+
+    for (int i = 0; i < input.Split(" ").Length; i += 2)
+    {
+      items.Add(int.Parse(input.Split(" ")[i]));
+
+      if (i != 0)
+      {
+        symbols.Add(input.Split(" ")[i - 1]);
+      }
+    }
+
+    foreach (var (i, value) in items.Index())
+    {
+      if (i == 0) 
+      { 
+        total = value; 
+      }
+      else if (symbols[i - 1] == "*")
+      {
+        total *= value;
+      }
+      else
+      {
+        total += value;
+      }
+    }
+    Console.WriteLine("Total was: " + total);
+    Console.WriteLine("Expected total was: 1120");
+    Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
+  }
+}
